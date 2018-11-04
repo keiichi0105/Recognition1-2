40c82e66913ecee249a8b6ae0e6b8a516317b557
keiichi <keiichi@cs16011>
2018-11-04 14:54:07 +0900

    recognition1

---
+++ src/recognition01_new/Recognition01_new_main.java
@@ -0,0 +1,58 @@
+package recognition01_new;
+
+
+
+import java.io.File;
+
+import java.io.FileNotFoundException;
+
+import java.io.IOException;
+
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions.Builder;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
+
+import recognition1.Recognition1_lib;
+
+
+
+
+
+public class Recognition01_new_main {
+
+
+
+	public static void main(String[] args) {
+         // TODO Auto-generated method stub
+
+		Recognition1_lib rlib = new Recognition1_lib();
+		String file = "img/prez.jpg";
+		DetectedFaces result = rlib.result(file);
+				
+rlib.Json(result);
+				
+
+				
+
+				
+
+				
+		
+
+	}
+
+
+
+}
